b8cee415c050e6f72cf67d165f9528534cd8efda
Sylvain FLINOIS <sflinois@e2r7p5.42.fr>
2017-04-30 16:40:07 +0200

    mtx4 mult fct

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: sflinois <sflinois@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 11:22:10 by sflinois          #+#    #+#             */
-/*   Updated: 2017/04/29 17:27:48 by sflinois         ###   ########.fr       */
+/*   Updated: 2017/04/30 16:38:00 by sflinois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,5 +132,8 @@ int					ft_printf(const char *format, ...);
 t_matrix			*ft_mtxnew(int nb_rows, int nb_cols, int **tab);
 t_matrix			*ft_mtxmult(t_matrix *a, t_matrix *b);
 t_matrix			*ft_mtxmultint(int a, t_matrix *b);
+int					**ft_mtx4_multmtx(int **a, int **b);
+int					**ft_mtx4_multvec(int **m, int **v);
+int					**ft_mtx4_multnb(int i, int **m);
 
 #endif
---
+++ srcs/ft_mtx4.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mtx4.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sflinois <sflinois@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/30 16:17:10 by sflinois          #+#    #+#             */
+/*   Updated: 2017/04/30 16:39:40 by sflinois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+#include <stdlib.h>
+
+int		**ft_mtx4_multmtx(int **a, int **b)
+{
+	int		**ret;
+	int		y;
+	int		x;
+
+	y = 0;
+	if ((ret = (int**)malloc(sizeof(int*) * (4))))
+		return (NULL);
+	while (y < 4)
+	{
+		if ((ret[y] = (int*)malloc(sizeof(int) * (4))))
+			return (NULL);
+		x = 0;
+		while (x < 4)
+		{
+			ret[y][x] = (a[y][0] * b[0][x]) + (a[y][1] * b[1][x])
+				+ (a[y][2] * b[2][x]) + (a[y][3] * b[3][x]);
+			x++;
+		}
+		y++;
+	}
+	return (ret);
+}
+
+int		**ft_mtx4_multvec(int **m, int **v)
+{
+	int		**ret;
+	int		y;
+
+	y = 0;
+	if ((ret = (int**)malloc(sizeof(int*) * (4))))
+		return (NULL);
+	while (y < 4)
+	{
+		if ((ret[y] = (int*)malloc(sizeof(int))))
+			return (NULL);
+		ret[y][0] = (m[y][0] * v[0][0]) + (m[y][1] * v[1][0])
+			+ (m[y][2] * v[2][0]) + (m[y][3] * v[3][0]);
+		y++;
+	}
+	return (ret);
+}
+
+int		**ft_mtx4_multnb(int i, int **m)
+{
+	int		**ret;
+	int		y;
+	int		x;
+
+	y = 0;
+	if ((ret = (int**)malloc(sizeof(int*) * (4))))
+		return (NULL);
+	while (y < 4)
+	{
+		if ((ret[y] = (int*)malloc(sizeof(int) * (4))))
+			return (NULL);
+		x = 0;
+		while (x < 4)
+		{
+			ret[y][x] = m[y][x] * i;
+			x++;
+		}
+		y++;
+	}
+	return (ret);
+}
+
